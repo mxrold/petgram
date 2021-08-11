db3b64477ff2717cfd85b2a9ed7060ebe530550f
Martin <martinroldan889@gmail.com>
2021-08-11 12:57:35 -0300

    [Add] Standard. Config linter and prettier. Deploy project and api  with vercel

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-    <h1>Hola Mundo!</h1>, document.getElementById('App')
+  <h1>Hola Mundo!</h1>, document.getElementById('App')
 )
