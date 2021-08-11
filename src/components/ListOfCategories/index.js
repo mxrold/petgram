4d80cfdfbede63969701d88c45d89297f0c2cb48
Martin <martinroldan889@gmail.com>
2021-08-11 15:49:29 -0300

    Data from API to show categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Category } from '../Category'
+import { categories } from '../../../api/db.json'
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   return (
     <List>
       {
-        [1, 2, 3].map(category =>
-          <Item key={category}>
-            <Category />
+        categories.map(category =>
+          <Item key={category.id}>
+            <Category {...category} />
           </Item>
         )
       }
