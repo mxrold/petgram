1c29515361eaf093ed59a91e5fc096c585acc292
Martin <martinroldan889@gmail.com>
2021-08-11 14:54:55 -0300

    [Add] Styled components. Component category. File App.js

---
+++ src/components/Category/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const Anchor = styled.a`
+    display: flex;
+    flex-direction: column;
+    width: 75px;
+    text-align: center;
+    text-decoration: none;
+`
+
+export const Image = styled.img`
+    height: 75px;
+    width: 75px;
+    overflow: hidden;
+    object-fit: cover;
+    box-shadow: 0px 10px 14px rgba(0, 0, 0, 0.2);
+    border: 1px solid #ddd;
+    border-radius: 50%;
+`
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './App'
 
 ReactDOM.render(
-  <h1>Hola Mundo!</h1>, document.getElementById('App')
+  <App />, document.getElementById('App')
 )
