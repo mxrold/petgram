0fce92c8c785555023017fc16949a06cb92de0fd
Martin <martinroldan889@gmail.com>
2021-08-12 12:03:19 -0300

    Fetch api categories list

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
-import { categories } from '../../../api/db.json'
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const fetchData = () => {
+      const api = 'https://petgram-server-mxrold-mxrold.vercel.app/categories'
+      window.fetch(api)
+        .then(res => res.json())
+        .then(response => {
+          setCategories(response)
+        })
+    }
+    
+    fetchData()
+  }, [])
+
   return (
     <List>
       {
