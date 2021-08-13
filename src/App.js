07202f91607379e806f223dd38382f27f81ac53d
Martin <martinroldan889@gmail.com>
2021-08-13 20:11:28 -0300

    Detail page created

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@
 /* eslint-disable indent */
 import React from 'react'
 import { Router } from '@reach/router'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 import { Logo } from './components/Logo'
 import { GlobalStyles } from './styles/GlobalStyles'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <>
       <GlobalStyles />
       <Logo />
-      {
-        detailId
-        ? <PhotoCardWithQuery id={detailId} />
-        : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </>
   )
 }
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from '@reach/router'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
@@ -17,11 +18,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       {
         showImage &&
           <>
-            <a href={`/?detail=${id}`}>
+            <Link to={`/detail/${id}`}>
               <Figure>
                 <Image src={src} />
               </Figure>
-            </a>
+            </Link>
             <ToggleLikeMutation>
               {
                 (toggleLike) => {
