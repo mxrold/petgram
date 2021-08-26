06eb0f088fb99e392aa2c6352cb18884c03de2e4
Martin <martinroldan889@gmail.com>
2021-08-26 13:15:31 -0300

    Improve routes

---
+++ src/pages/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const NotFound = () => (
+  <div>
+    <h1>Page not found</h1>
+    <img src='https://i.ibb.co/hDf5jv3/this-is-fine-404.gif' />
+  </div>
+)
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,21 +1,28 @@
 import React, { useContext } from 'react'
-import { Router } from '@reach/router'
+import { Redirect, Router } from '@reach/router'
+import { Context } from '../context/Context'
+import { Favs } from '../pages/Favs'
+import { User } from '../pages/User'
+import { NotRegisteredUser } from '../pages/NotRegisteredUser'
 import { Home } from '../pages/Home'
 import { Detail } from '../pages/Detail'
-import { UserLogged } from './UserLogged'
-import { Context } from '../context/Context'
+import { NotFound } from '../pages/NotFound'
 
 export const Route = () => {
   const { isAuth } = useContext(Context)
 
   return (
-    <>
-      <Router>
-        <Home path='/' />
-        <Home path='/pet/:id' />
-        <Detail path='/detail/:detailId' />
-      </Router>
-      <UserLogged isAuth={isAuth} />
-    </>
+    <Router>
+      <NotFound default />
+      <Home path='/' />
+      <Home path='/pet/:id' />
+      <Detail path='/detail/:detailId' />
+      {!isAuth && <NotRegisteredUser path='/login' />}
+      {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+      {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+      {isAuth && <Redirect from='/login' to='/' />}
+      <Favs path='/favs' />
+      <User path='/user' />
+    </Router>
   )
 }
